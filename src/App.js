88a5d9a5b546f6059a691a3d4a841d428e1f1740
ZackNemec <zacknemec@live.com>
2020-07-30 18:08:20 -0700

    still working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    axios.get("https://api.nasa.gov/#apod").then((pull) => {
+      console.log(pull.data);
+      setData(pull.data.results);
+    });
+  }, []);
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
